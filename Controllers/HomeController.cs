3aed36aacc267df03fbf27b5aac1023f8a164466
asp <asp@aptech.com>
2024-06-01 17:01:00 +0500

    Added image crud

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
 using System.Diagnostics;
 using viewbag.Models;
 
@@ -41,16 +42,52 @@ namespace viewbag.Controllers
         }
         public IActionResult product()
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
             return View();
         }
         [HttpPost]
-        public IActionResult product(Product product,IFormFile img)
+        public IActionResult product(Product product, IFormFile img)
         {
-           if(img != null && img.Length > 1)
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                { 
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+            if (img != null && img.Length > 1)
             {
                 var filetype = System.IO.Path.GetExtension(img.FileName).Substring(1);
 
-                if (filetype == "png" || filetype == "jpeg")
+                if (filetype == "png" || filetype == "jpeg" || filetype == "jfif")
                 {
                     var realname = Path.GetFileName(img.FileName);
 
@@ -89,71 +126,224 @@ namespace viewbag.Controllers
                     ViewBag.failedfile = "File type is not supported";
                 }
             }
-
-            
             return View();
         }
         public IActionResult productlist()
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
             var show = context.Products.ToList();
             return View(show);
         }
         public IActionResult productedit(int id)
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
             var show = context.Products.Find(id);
             return View(show);
         }
         [HttpPost]
         public IActionResult productedit(int id,Product product,IFormFile img)
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
             var finduser = context.Products.FirstOrDefault(options => options.Id == id);
-            //if (img != null && img.Length > 1)
-            //{
-            //    var filetype = System.IO.Path.GetExtension(img.FileName).Substring(1);
+            if (img != null && img.Length > 1)
+            {
+                var filetype = System.IO.Path.GetExtension(img.FileName).Substring(1);
 
-            //    if (filetype == "png" || filetype == "jpeg" || filetype == "jfif")
-            //    {
-            //        var realname = Path.GetFileName(img.FileName);
+                if (filetype == "png" || filetype == "jpeg" || filetype == "jfif")
+                {
+                    var realname = Path.GetFileName(img.FileName);
 
-            //        var name = Guid.NewGuid() + realname;
+                    var name = Guid.NewGuid() + realname;
 
-            //        var folder = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/images");
+                    var folder = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/images");
 
-            //        if (!Directory.Exists(folder))
-            //        {
-            //            Directory.CreateDirectory(folder);
-            //        }
+                    if (!Directory.Exists(folder))
+                    {
+                        Directory.CreateDirectory(folder);
+                    }
 
-            //        var path = Path.Combine(folder, name);
+                    var path = Path.Combine(folder, name);
 
-            //        using (var stream = new FileStream(path, FileMode.Create))
-            //        {
-            //            img.CopyTo(stream);
-            //        }
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        img.CopyTo(stream);
+                    }
 
-            //        var dbPath = Path.Combine("images", name);
+                    var dbPath = Path.Combine("images", name);
 
-            //        Product data = new Product()
-            //        {
-            //            Name = product.Name,
-            //            Price = product.Price,
-            //            Quantity = product.Quantity,
-            //            Discription = product.Discription,
-            //            Image = dbPath
-            //        };
-            //        context.Products.Update(data);
-            //        context.SaveChanges();
-            //        return RedirectToAction("productlist");
-            //    }
-            //    else
-            //    {
-            //        ViewBag.failedfile = "File type is not supported";
-            //    }
-            //}
+
+                    finduser.Name = finduser.Name;
+                    finduser.Price = finduser.Price;
+                    finduser.Quantity = finduser.Quantity;
+                    finduser.Discription = finduser.Discription;
+                    finduser.Image = dbPath;
+
+
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else if(finduser.Name != null)
+                {
+                    finduser.Name = finduser.Name;
+                    finduser.Price = finduser.Price;
+                    finduser.Quantity = finduser.Quantity;
+                    finduser.Discription = finduser.Discription;
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else if (finduser.Price != null)
+                {
+                    finduser.Name = finduser.Name;
+                    finduser.Price = finduser.Price;
+                    finduser.Quantity = finduser.Quantity;
+                    finduser.Discription = finduser.Discription;
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else if (finduser.Quantity != null)
+                {
+                    finduser.Name = finduser.Name;
+                    finduser.Price = finduser.Price;
+                    finduser.Quantity = finduser.Quantity;
+                    finduser.Discription = finduser.Discription;
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else if (finduser.Discription != null)
+                {
+                    finduser.Name = finduser.Name;
+                    finduser.Price = finduser.Price;
+                    finduser.Quantity = finduser.Quantity;
+                    finduser.Discription = finduser.Discription;
+                    context.SaveChanges();
+                    return RedirectToAction("productlist");
+                }
+                else
+                {
+                    ViewBag.failedfile = "File type is not supported";
+                }
+            }
             return View();
+        }
+        public IActionResult deleteproduct(int id)
+        {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+            var show = context.Products.Find(id);
+            return View(show);
+        }
+        [HttpPost]
+        public IActionResult deleteproduct(int id,Product product)
+        {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return View();
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View("Login");
+            }
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("productlist");
         }
         public IActionResult signup()
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View();
+            }
             CustormUser newdata = new CustormUser()
             {
                 CUser = new User(),
@@ -177,6 +367,25 @@ namespace viewbag.Controllers
         }
         public IActionResult login()
         {
+            var asd = accessor.HttpContext.Session.GetString("sessionName");
+            if (accessor.HttpContext.Session.GetString("sessionName") != null)
+            {
+                ViewBag.sessionName = asd;
+                if (accessor.HttpContext.Session.GetString("SessionRole") == "Customer")
+                {
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (accessor.HttpContext.Session.GetString("SessionRole") == "Admin")
+                {
+                    return RedirectToAction("Index", "Admin");
+                }
+            }
+            else
+            {
+                return View();
+            }
+
             return View();
         }
         [HttpPost]
